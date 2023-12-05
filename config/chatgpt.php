de024f3734466e7ef27ff46e2eff05ccd3f21cbd
dhanar98 <dhanasekarravi98@gmail.com>
2023-12-05 22:00:24 +0530

    chatgpt configuration added.

---
+++ config/chatgpt.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CHATGPT API KEY
+    |--------------------------------------------------------------------------
+    |
+    | This configuration key is used to store the ChatGPT API key for the application.
+    | The API key is utilized when making requests to the ChatGPT service.
+    | You can set the key in your environment variables or directly in this configuration file.
+    |
+    */
+
+    'chatgpt_api_key' => env('CHATGPT_API_KEY', ''),
+];
